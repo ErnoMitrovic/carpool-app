d129fa877d4023c2cfeaffe40d616dd314c250d6
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2025-02-25 20:52:19 +0100

    Cleaning project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,36 @@
 import { StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CarpoolMap } from '@/components/CarpoolMap'
+import { StatusBar } from 'expo-status-bar'
+import { MapMarker, Region } from 'react-native-maps'
+import { getCurrentPositionAsync, useForegroundPermissions } from 'expo-location'
+import { API_URL } from '@/services'
 
 const RideScreen = () => {
+  const [markers, setMarkers] = useState<MapMarker[]>([]);
+  const [initialRegion, setInitialRegion] = useState<Region>();
+  const [status, setStatus] = useForegroundPermissions();
+
+  useEffect(() => {
+    if (status?.granted) {
+      getCurrentPositionAsync().then((location) => {
+        setInitialRegion({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        });
+      });
+    } else {
+      setStatus();
+    }
+  }, [status]);
+
   return (
-    <CarpoolMap />
+    <>
+      <CarpoolMap initialRegion={initialRegion} markers={markers} />
+      <StatusBar hidden={true} />
+    </>
   )
 }
 

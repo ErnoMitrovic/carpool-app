4fc9c92bf6a5b5c2ec459aa078d31ec5430b094b
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2025-03-05 18:11:03 +0100

    Styling finished

--- components/CarpoolTabBar/CarpoolTabBar.tsx
+++ components/CarpoolTabBar/CarpoolTabBar.tsx
@@ -1,21 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { PlatformPressable } from '@react-navigation/elements';
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { BottomNavigation } from 'react-native-paper';
+import { FC } from 'react';
+import { CommonActions } from '@react-navigation/native';
 
-const CarpoolTabBar = (props: BottomTabBarButtonProps) => {
+const CarpoolTabBar: FC<BottomTabBarProps> = ({ state, navigation, descriptors, insets }) => {
   return (
-      <PlatformPressable
-        {...props}
-        onPressIn={(ev) => {
-          if (process.env.EXPO_OS === 'ios') {
-            impactAsync(ImpactFeedbackStyle.Light);
-          }
-          props.onPressIn?.(ev);
-        }}
-      />
-    );
+    <BottomNavigation.Bar
+      navigationState={state}
+      safeAreaInsets={insets}
+      onTabPress={({ route, preventDefault }) => {
+        const event = navigation.emit({
+          type: 'tabPress',
+          target: route.key,
+          canPreventDefault: true,
+        });
+
+        if (event.defaultPrevented) {
+          preventDefault();
+        } else {
+          navigation.dispatch({
+            ...CommonActions.navigate(route.name, route.params),
+            target: state.key,
+          });
+        }
+      }}
+      renderIcon={({ route, focused, color }) => {
+        const { options } = descriptors[route.key];
+        if (options.tabBarIcon) {
+          return options.tabBarIcon({ focused, color, size: 24 });
+        }
+
+        return null;
+      }}
+      getLabelText={({ route }) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel.toString()
+            : options.title !== undefined
+              ? options.title
+              : route.name;
+
+        return label;
+      }}
+    />);
 }
 
 export default CarpoolTabBar

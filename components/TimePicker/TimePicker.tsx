8b6d1f66fa9abd5e675b40a2c288a2a9544cd177
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2025-03-05 18:11:03 +0100

    Styling finished

---
+++ components/TimePicker/TimePicker.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TimePicker = () => {
+
+    return (
+        <View>
+            <Text>TimePicker</Text>
+        </View>
+    )
+}
+
+export default TimePicker
+
+const styles = StyleSheet.create({})

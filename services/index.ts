19df8e37c1fee6c961bc1c6be65026a96ec22cf9
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2025-03-12 10:42:56 +0100

    Sign up finished

--- services/index.ts
+++ services/index.ts
@@ -1,1 +1,3 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8080/api/v1';
+import Constants from 'expo-constants';
+
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || `http://${Constants.expoConfig?.hostUri?.split(':')[0]}:8080/api/1.0.0`;
